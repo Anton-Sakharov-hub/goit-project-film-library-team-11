0b67e7581a1425bd15721e0e0809baefcfacc640
Anton-Sakharov-hub <63040637+Anton-Sakharov-hub@users.noreply.github.com>
2021-10-10 21:22:14 +0000

    Deploying to gh-pages from @ Anton-Sakharov-hub/goit-project-film-library-team-11@9b394c1f934bde1dea95ff4e6148f4fe97a1d8b0 🚀

--- src.901acb3d.js
+++ src.901acb3d.js
@@ -7,4 +7,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"../js/refs.js":"VyiV"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/header.js");
 },{"./sass/main.scss":"clu1","./js/header.js":"cGsR"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-project-film-library-team-11/src.3ab073fa.js.map
+//# sourceMappingURL=/goit-project-film-library-team-11/src.901acb3d.js.map
