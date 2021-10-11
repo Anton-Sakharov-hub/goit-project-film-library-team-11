1619093ac73bb71ab1909bb82cb65a86778db209
Anton-Sakharov-hub <saharovanton357@gmail.com>
2021-10-11 15:00:44 +0300

    создает разметку и стили футера
    - создает разметку;
    - создает стили;
    - находит в js ссылку в в футере;
    - добавляет иконку;
    - добавляет секцию main и свойство высоты
    - меняет высоту строки mixin-а font-syles

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,10 +1,11 @@
 export default {
-  // ---------хедер--------
+  // --------- header --------
   header: document.querySelector('.header-js'),
   btnHome: document.querySelector('.btn-home-js'),
   btnMyLibrary: document.querySelector('.btn-myLibrary-js'),
   btnSubmit: document.querySelector('.btn-submit-js'),
   formSearch: document.querySelector('.form-js'),
   navLibrary: document.querySelector('.nav-library-js'),
-  // ----------------------
+  // -------- footer --------------
+  teamModalBtn: document.querySelector('.js-teamModalBtn')
 };
