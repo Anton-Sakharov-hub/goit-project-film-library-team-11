492f68574664d7f06af286a4b31cb156660decc9
Anton-Sakharov-hub <saharovanton357@gmail.com>
2021-10-13 22:28:19 +0300

    создает функции для работы с local storage

--- src/js/homeRendering.js
+++ src/js/homeRendering.js
@@ -6,8 +6,37 @@ export default function homeRendering() {
     .then(({ results }) => {
       console.log(results);
       requests.createMarkup(results);
+      // добавляет в local storage
+      addFilmsToLclStorage(results);
     })
     .catch(err => console.log(err));
 }
 
 homeRendering();
+
+// логика добавления на local storge 
+
+function addFilmsToLclStorage(ResponseRusult) {
+  const localFilmsData = JSON.stringify({filmsArr: ResponseRusult});
+  localStorage.setItem('trending_films', localFilmsData);
+}
+
+function getFilmsFromLclStorage(key) {
+  const localFilmsData = JSON.parse(localStorage.getItem(key));
+  const { filmsArr } = localFilmsData;
+
+  return filmsArr;
+}
+
+// функция для поиска фильма по id
+
+function findFilm (filmId, filmsArray) {
+  const filmData = filmsArray.find(film => film.id === filmId);
+
+  return filmData;
+}
+
+const filmsArray = getFilmsFromLclStorage('trending_films'); // это массив фильмов с local storage
+
+// id = 550988;
+console.log(findFilm(550988, filmsArray));
