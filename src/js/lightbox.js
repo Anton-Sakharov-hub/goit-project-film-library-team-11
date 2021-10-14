b5b9f003f0203424797e0af25ad597b515e5682a
MykytaPervushyn <80350205+MykytaPervushyn@users.noreply.github.com>
2021-10-14 18:23:31 +0300

    Зберігає зміни

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,4 +1,7 @@
+import lightboxTpl from '../templates/lightbox.hbs';
+import localStorage from '../js/local_storage';
 import refs from '../js/refs.js';
+
 
 const { cardsContainer, lightboxContainer, backdrop, closeBtn } = refs;
 
@@ -6,8 +9,6 @@ cardsContainer.addEventListener('click', onCardsContainerClick);
 backdrop.addEventListener('click', onBackdropClick);
 
 function onCardsContainerClick(e) {
-    window.addEventListener('keydown', onEscKeyPress, { once: true })
-  closeBtn.addEventListener('click', onCloseLightbox, { once: true });
 
   e.preventDefault()
 
@@ -19,6 +20,25 @@ function onCardsContainerClick(e) {
 
     console.log(e.target.nodeName);
     lightboxContainer.classList.add('is-open');
+
+      const filmId =  Number(e.target.dataset.filmid)
+        // renderModalCard(data);
+  // console.log(e.target.dataset.filmid);
+
+  const filmsArray = localStorage.getLocalStorage('Query'); // это массив фильмов с local storage
+
+// id = 550988;
+  const dataFilm = findFilm(filmId, filmsArray);
+  const markup = renderModalCard(dataFilm);
+
+
+  lightboxContainer.insertAdjacentHTML = markup;
+
+  
+    window.addEventListener('keydown', onEscKeyPress, { once: true })
+  closeBtn.addEventListener('click', onCloseLightbox, { once: true });
+
+      console.log(filmsArray);
     
 }
 
@@ -31,11 +51,32 @@ function onCloseLightbox(e) {
 
 
 function onBackdropClick(e) {
-   onCloseLightbox(e)
+  if (e.target === e.currentTarget) {
+    onCloseLightbox(e);
+  }
 }
 
 function onEscKeyPress(e) {
   if (e.code === 'Escape') {
     onCloseLightbox(e)
   }
+}
+
+function renderModalCard(data) {
+  lightboxContainer.insertAdjacentHTML('beforeend', lightboxTpl(data));
+}
+
+
+function findFilm (filmId, filmsArray) {
+  
+  // console.log(filmId);
+  
+  const filmData = filmsArray.find(film => filmId === film.id
+  );
+
+
+
+  return filmData;
+
+  
 }
