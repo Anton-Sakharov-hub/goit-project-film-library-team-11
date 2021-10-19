9f7af075bafb934f1282d46a33dde29c1fa12de8
Prohnov Pavel <54280007+pr0xnov@users.noreply.github.com>
2021-10-20 02:40:00 +0300

    Изменил запрос

--- src/js/searchMovieRendering.js
+++ src/js/searchMovieRendering.js
@@ -4,6 +4,7 @@ import LS from './local_storage.js';
 import { togleClass, createMarkup, clearInput } from './commonFunction.js';
 import GenresDataWork from './GenresDataWork';
 import { searchMoviePagination } from './pagination-btns';
+import trendingFetch from './homeRendering';
 
 //импорт toastr notification
 import toastr from 'toastr';
@@ -37,18 +38,20 @@ function onFormSearchsubmit(e) {
   const input = e.currentTarget.elements.query;
   e.preventDefault();
   updateQuery(input.value);
-
+  
   if (input.value.trim() == '') {
     toastr.warning('Пожалуйста, введите ваш запроc');
     clearInput(input);
   }
+
   requests
     .movieFetch()
     .then(({ results, total_results }) => {
+      
       if (results.length < 1) {
-        clearInput(input);
         toastr.error('Фильм не найден! Измените ввод и повторите попытку');
-        apiServise.query = '';
+        clearInput(input);
+        trendingFetch();
       }
       createMarkup(results);
       clearInput(input);
